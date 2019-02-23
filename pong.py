6751c1e148720c9894bf5145ac75886a86b4b7a7
A.L. Blackwell <ahvblackwelltech@gmail.com>
2019-02-22 18:33:03 -0800

    Fixed Bug

--- pong.py
+++ pong.py
@@ -33,6 +33,8 @@ ball.shape("circle")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 2
+ball.dy = 2
 
 # Function
 def paddle_a_up():
@@ -40,17 +42,31 @@ def paddle_a_up():
     y += 20
     paddle_a.sety(y)
 
-
 def paddle_a_down():
     y = paddle_a.ycor()
     y -= 20
     paddle_a.sety(y)
 
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
 # Keyboard binding
 wn.listen()
 wn.onkeypress(paddle_a_up, "w")
-wn.onkeypress(paddle_a_up, "s")
+wn.onkeypress(paddle_a_down, "s")
+wn.onkeypress(paddle_b_up, "Up")
+wn.onkeypress(paddle_b_down, "Down")
 
 # Main Game Loop
 while True: 
     wn.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
