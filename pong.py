66b890863b100f1b9f4f6494021d35143c97073d
A.L. Blackwell <ahvblackwelltech@gmail.com>
2019-02-22 17:45:57 -0800

    Fixed Bug

--- pong.py
+++ pong.py
@@ -7,3 +7,20 @@ wn.title("Ping Pong by @alblackwelldev")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+
+# Paddle B
+
+
+# Ball
+
+
+
+# Main Game Loop
+while True: 
+    wn.update()
