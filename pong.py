1e0874fe85d412775d4e98dad3f074ddc00efc0c
A.L. Blackwell <ahvblackwelltech@gmail.com>
2019-02-23 19:18:25 -0800

    Fixed Bug

--- pong.py
+++ pong.py
@@ -10,6 +10,10 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Score
+score_a = 0
+score_b = 0
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -37,6 +41,16 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 2
 ball.dy = -2
+
+# Pen 
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
 
 # Function
 def paddle_a_up():
@@ -88,18 +102,26 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0,0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
         os.system("afplay bounce.wav&")
-
+        
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
-        os.system("afplay bounce.wav&")
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+        os.system("afplay Jump.wav&")
 
 # Paddle and Ball Collisions
     if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
+        ball.setx(-340)
         ball.dx *= -1
-        os.system("afplay bounce.wav&")
+        os.system("afplay Jump.wav&")
 
     elif ball.xcor() > 340 and ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50:
+        ball.setx(340)
         ball.dx *= -1
-        os.system("afplay bounce.wav&")
+        os.system("afplay Jump.wav&")
